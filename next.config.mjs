9641d9c6b4c6f19138224dcf3644c3964001ca61
Aldiyar <nesquikyey@gmail.com>
2025-12-05 13:13:21 +0500

    change next config

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.js');
 
 const nextConfig = {
   reactCompiler: true,
+  output: 'standalone'
 };
 
 export default withNextIntl(nextConfig);

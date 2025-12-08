95ab0047e0d4fddf464b98a8cac9f1ebebc1edb5
Aldiyar <nesquikyey@gmail.com>
2025-12-08 11:45:15 +0500

    removed output standalone

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.js');
 
 const nextConfig = {
   reactCompiler: true,
-  output: 'standalone'
 };
 
 export default withNextIntl(nextConfig);
